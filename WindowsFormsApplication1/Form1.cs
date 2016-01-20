bb5800542221faf9dee8f89e9df9a251be816169
Ngoc TDA <ngoctda@live.com>
2016-01-20 11:25:14 +0700

    ver 1

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApplication1.ServiceReference1;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            try
+            {
+                ServiceReference1.ServiceClient client = new ServiceReference1.ServiceClient();
+                bool querySql = true;
+                client.querySql(out querySql);
+
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+        }
+    }
+}
